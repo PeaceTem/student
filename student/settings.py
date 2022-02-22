58c47cc22e0ed73e379a32d52710409f83fd99e7
PeaceTem <olumidejonathan10@gmail.com>
2022-02-22 05:00:37 +0100

    add env and sqlite

--- student/settings.py
+++ student/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ["https://ecclewikitestquiz.herokuapp.com/", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["https://ecclewikitestquiz.herokuapp.com/", "127.0.0.1", "localhost", "15.237.100.157"]
 
 
 # Application definition
